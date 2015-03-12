899f1bf57d66f3ddc2fc8902cf447491922d9831
Matt Edwards <edwardsjm01@gmail.com>
2015-03-12 16:31:39 -0600

    added function bodies to server test gui

--- test/servertester/controllers/Controller.java
+++ test/servertester/controllers/Controller.java
@@ -2,7 +2,11 @@ package servertester.controllers;
 
 import java.util.*;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import servertester.views.*;
+import shared.Communicator.ClientCommunicator;
+import shared.Params.*;
 
 public class Controller implements IController {
 
@@ -70,6 +74,11 @@ public class Controller implements IController {
 
 	@Override
 	public void executeOperation() {
+        if(!getView().getPort().matches("^[0-9]+$")){
+            getView().setResponse("FAILED\n");
+            return;
+        }
+        ClientCommunicator.getInstance().setPort(Integer.parseInt(getView().getPort()));
 		switch (getView().getOperation()) {
 		case VALIDATE_USER:
 			validateUser();
@@ -99,24 +108,64 @@ public class Controller implements IController {
 	}
 	
 	private void validateUser() {
+        String[] paramValues = getView().getParameterValues();
+        ValidateUser_Params params = new ValidateUser_Params(paramValues[0],paramValues[1]);
+        getView().setRequest(new XStream(new DomDriver()).toXML(params));
+        getView().setResponse(ClientCommunicator.getInstance().validateUser(params).toString());
 	}
 	
 	private void getProjects() {
+        String[] paramValues = getView().getParameterValues();
+        GetProjects_Params params = new GetProjects_Params(paramValues[0],paramValues[1]);
+        getView().setRequest(new XStream(new DomDriver()).toXML(params));
+        getView().setResponse(ClientCommunicator.getInstance().getProjects(params).toString());
 	}
 	
 	private void getSampleImage() {
+        String[] paramValues = getView().getParameterValues();
+        GetSampleImage_Params params = new GetSampleImage_Params(paramValues[0],paramValues[1],Integer.parseInt(paramValues[2]));
+        getView().setRequest(new XStream(new DomDriver()).toXML(params));
+        getView().setResponse(ClientCommunicator.getInstance().getSampleImage(params).toString());
 	}
 	
 	private void downloadBatch() {
+        String[] paramValues = getView().getParameterValues();
+        DownloadBatch_Params params = new DownloadBatch_Params(paramValues[0],paramValues[1],Integer.parseInt(paramValues[2]));
+        getView().setRequest(new XStream(new DomDriver()).toXML(params));
+        getView().setResponse(ClientCommunicator.getInstance().downloadBatch(params).toString());
 	}
 	
 	private void getFields() {
+        String[] paramValues = getView().getParameterValues();
+        GetFields_Params params = new GetFields_Params(paramValues[0],paramValues[1],Integer.parseInt(paramValues[2]));
+        getView().setRequest(new XStream(new DomDriver()).toXML(params));
+        getView().setResponse(ClientCommunicator.getInstance().getFields(params).toString());
 	}
 	
 	private void submitBatch() {
+        String[] paramValues = getView().getParameterValues();
+        SubmitBatch_Params params = new SubmitBatch_Params();
+        params.setUsername(paramValues[0]);
+        params.setPassword(paramValues[1]);
+        params.setBatchId(Integer.parseInt(paramValues[2]));
+        ArrayList<String[]> recordVals = new ArrayList<String[]>();
+        String[] records = paramValues[3].split(";");
+        for(String valueList:records){
+            String[] values = valueList.split(",");
+            recordVals.add(values);
+        }
+        params.setFieldValues(recordVals);
+        getView().setRequest(new XStream(new DomDriver()).toXML(params));
+        getView().setResponse(ClientCommunicator.getInstance().submitBatch(params).toString());
 	}
 	
 	private void search() {
+        String[] paramValues = getView().getParameterValues();
+        String[] fields = paramValues[2].split(",");
+        String[] searchVals = paramValues[3].split(",");
+        Search_Params params = new Search_Params(paramValues[0],paramValues[1],searchVals,fields);
+        getView().setRequest(new XStream(new DomDriver()).toXML(params));
+        getView().setResponse(ClientCommunicator.getInstance().search(params).toString());
 	}
 
 }
